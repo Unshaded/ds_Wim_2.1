857377490af2811dcf94b3b97287391b7704303d
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-06-15 03:40:46 -0400

    question 1 reussie

---
+++ ex1/message.php
@@ -0,0 +1,11 @@
+<?php
+extract($_POST);
+if($langue){
+  if($langue=='anglais')
+    echo'Hello';
+  if($langue=='francais')
+    echo 'Bonjour';
+  if($langue=='espagnol'){
+    echo 'Hola';
+  }
+}
