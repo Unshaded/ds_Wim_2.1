fffa843d2e846a4c676ee1c1cf18c38e6b3d828a
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-06-15 05:51:49 -0400

    les cookies ne marchent pas

--- ex1/message.php
+++ ex1/message.php
@@ -1,11 +1,23 @@
 <?php
 extract($_POST);
 if($langue){
-  if($langue=='anglais')
+  if($langue=='anglais'){
+    //setcookie('1', true,); ne marche pas
+    //setcookie('2', false);
+    //setcookie('3', false);
     echo'Hello';
-  if($langue=='francais')
+}
+  if($langue=='francais'){
+    //setcookie('2', true);
+    //setcookie('1', false);
+    //setcookie('3', false);
     echo 'Bonjour';
-  if($langue=='espagnol')
+  }
+  if($langue=='espagnol'){
+    //setcookie('3', true);
+    //setcookie('2', false);
+    //setcookie('1', false);
     echo 'Hola';
+  }
 }
 ?>
